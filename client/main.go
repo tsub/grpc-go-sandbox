698761a35ba11f77f2e82e88c7b88902e552ea56
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-02-18 10:20:03 +0900

    Implement gRPC client app
    And modify .gitignore

---
+++ client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	pb "github.com/tsub/grpc-go-sandbox/protobuf"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
